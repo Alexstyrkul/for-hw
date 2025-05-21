a8c095afa3b899240edb7b0b303c8c4f28f79261
Oleksandr Styrkul <alexroast3@gmail.com>
2025-05-21 17:32:43 +0300

    Домашнє завдання: task1 і task2

---
+++ task1_2.js
@@ -0,0 +1,6 @@
+const arr = [4, 4, 8, 3, 3, 3, 2, 4, 4];
+
+console.log("Перші три елементи:");
+for (let i = 0; i < 3; i++) {
+  console.log(arr[i]);
+}
---
+++ task1_3.js
@@ -0,0 +1,8 @@
+const arr = [4, 4, 8, 3, 3, 3, 2, 4, 4];
+
+let sum = 0;
+arr.forEach((el) => {
+  sum += el;
+});
+
+console.log("Сума всіх елементів:", sum);
