1f7505d5f214c5d2a8f82e2c0973caf6047e49be
Oleksandr Styrkul <alexroast3@gmail.com>
2025-06-02 14:39:02 +0300

    1439

--- clickup-goal-api-tests/cypress.config.ts
+++ clickup-goal-api-tests/cypress.config.ts
@@ -1,15 +1,18 @@
-import { defineConfig } from 'cypress';
-import dotenv from 'dotenv';
+import { defineConfig } from 'cypress'
+import * as dotenv from 'dotenv'
 
-dotenv.config();
+dotenv.config()
 
 export default defineConfig({
   e2e: {
-    baseUrl: process.env.BASE_URL,
-    env: {
-      token: process.env.TOKEN,
-      teamId: process.env.TEAM_ID,
-    },
+    baseUrl: process.env.BASE_URL || 'https://api.clickup.com/api/v2',
+    specPattern: 'cypress/e2e/**/*.cy.{js,jsx,ts,tsx}',
     supportFile: false,
-  },
-});
+    env: {
+      clickUpToken: process.env.CLICKUP_TOKEN,
+      workspaceId: process.env.WORKSPACE_ID,
+      goalFolderId: process.env.GOAL_FOLDER_ID
+    }
+  }
+})
+
